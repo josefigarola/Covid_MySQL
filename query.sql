905baadd425502f578cae60e2ae3f4b71cd00514
josefigarola <jmfigarolaprado2@gmail.com>
2023-08-28 17:01:36 -0600

    total covid

--- query.sql
+++ query.sql
@@ -1,3 +1,5 @@
+# Data Link: https://ourworldindata.org/covid-deaths
+# Changed date format to YYYY-MM-DD
 use covid;
 
 # Get deaths 
@@ -10,5 +12,15 @@ select location, date, total_cases, new_cases, total_deaths, population
 from deaths order by 1,2;
 
 # Total cases vs Total deaths
-select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
-as ratio from deaths order by 1,2;
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
+AS DeathsPercentage 
+FROM deaths
+WHERE location like 'Mexico'
+ORDER BY 1,2; 
+
+# Total cases vs population
+SELECT location, date, total_cases, population, (total_cases/population)*100
+AS TotalCovidPercentage 
+FROM deaths
+WHERE location like 'Mexico'
+ORDER BY 1,2; 

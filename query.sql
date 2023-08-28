b2ecfb89d1184f41dedbf528dfc556548477f992
josefigarola <jmfigarolaprado2@gmail.com>
2023-08-28 11:56:27 -0600

    get deaths vs cases ratio

--- query.sql
+++ query.sql
@@ -5,4 +5,10 @@ select * from deaths order by 3,4 limit 10;
 # Get vaccs
 select * from vaccs order by 3,3 limit 10;
 
-#Select the data 
+# Select the data 
+select location, date, total_cases, new_cases, total_deaths, population 
+from deaths order by 1,2;
+
+# Total cases vs Total deaths
+select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
+as ratio from deaths order by 1,2;

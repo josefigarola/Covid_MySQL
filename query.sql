cdea25a74b448cd331608a3d05a91bf3fd570dc4
josefigarola <jmfigarolaprado2@gmail.com>
2023-08-28 18:20:53 -0600

    show by continent

--- query.sql
+++ query.sql
@@ -2,9 +2,13 @@
 # Changed date format to YYYY-MM-DD
 use covid;
 
-# Get deaths 
+# Get data
+select * from deaths
+where continent is not null;
+
+# Get deaths data
 select * from deaths order by 3,4 limit 10;
-# Get vaccs
+# Get vaccs data
 select * from vaccs order by 3,3 limit 10;
 
 # Select the data 
@@ -12,15 +16,65 @@ select location, date, total_cases, new_cases, total_deaths, population
 from deaths order by 1,2;
 
 # Total cases vs Total deaths
-SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
-AS DeathsPercentage 
-FROM deaths
-WHERE location like 'Mexico'
-ORDER BY 1,2; 
+SELECT 
+    location, date, 
+    total_cases, total_deaths, 
+    (total_deaths/total_cases)*100 AS DeathsPercentage 
+FROM 
+    deaths
+WHERE 
+    location like 'Mexico'
+ORDER BY 
+    1,2; 
 
 # Total cases vs population
-SELECT location, date, total_cases, population, (total_cases/population)*100
-AS TotalCovidPercentage 
-FROM deaths
-WHERE location like 'Mexico'
-ORDER BY 1,2; 
+SELECT 
+    location, date, 
+    total_cases, population, 
+    (total_cases/population)*100 AS TotalCovidPercentage 
+FROM 
+    deaths
+WHERE 
+    location like 'Mexico'
+ORDER BY 
+    1,2; 
+
+# Get highest infection rates
+SELECT 
+    location, population, 
+    MAX(total_cases) AS HighestInfenctionCount,
+    MAX((total_cases/population)*100) AS TotalCovidPercentage 
+FROM 
+    deaths
+WHERE 
+    continent is not null
+GROUP BY 
+    location, population
+ORDER BY 
+    TotalCovidPercentage DESC; 
+
+# Show the countries with highest death count per population
+SELECT 
+    location, 
+    MAX(cast(total_deaths AS UNSIGNED)) AS TotalDeathCount
+FROM 
+    deaths
+WHERE 
+    continent is not null
+GROUP BY
+    location
+ORDER BY 
+    TotalDeathCount DESC; 
+
+# Show by continent
+SELECT 
+    continent, 
+    MAX(cast(total_deaths AS UNSIGNED)) AS TotalDeathCount
+FROM 
+    deaths
+WHERE 
+    continent is not null
+GROUP BY
+    continent
+ORDER BY 
+    TotalDeathCount DESC; 

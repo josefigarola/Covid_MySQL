58a50dbb3daef821cf43c847ccad484ea9eda64a
josefigarola <jmfigarolaprado2@gmail.com>
2023-09-04 17:47:47 -0600

    Graph Results with Python

---
+++ Utils/Graph.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from matplotlib.dates import AutoDateLocator
+
+class Graph:
+    def __init__(self, dates, values, location):
+        self.dates = dates
+        self.values = values
+        self.location = location
+        
+    def create_graph(self):
+        # Create a plot using matplotlib
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.plot(self.dates, self.values, linestyle='-', color='b')
+
+        # Use AutoDateLocator to automatically choose tick positions
+        ax.xaxis.set_major_locator(AutoDateLocator())
+
+        # Set the tick interval to increase spacing between date labels
+        ax.yaxis.set_major_locator(AutoDateLocator(minticks=30))  # Adjust minticks as needed
+
+        # Labeling and formatting
+        plt.title('Deaths per day in Mexico')
+        plt.xlabel('Date')
+        plt.ylabel('Value')
+
+        # Save the plot to a file (optional)
+        name = 'Deaths per day in' + self.location
+        plt.savefig('Others/'+name+'.png')
+
+        # Show the plot
+        plt.show()
